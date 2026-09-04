770bcc2356fd8c6cc312af98a2f2d27b1883ca7e
Laila1303 <laila.240180177@mhs.unimal.ac.id>
2026-09-05 06:36:35 +0700

    Update storage env in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 
-// Buat folder storage sementara di /tmp
-$dirs = [
+// Pastikan folder writable dibuat di /tmp
+$storagePaths = [
     '/tmp/storage/framework/views',
     '/tmp/storage/framework/cache/data',
     '/tmp/storage/framework/sessions',
@@ -9,18 +9,19 @@ $dirs = [
     '/tmp/bootstrap/cache',
 ];
 
-foreach ($dirs as $dir) {
-    if (!is_dir($dir)) {
-        mkdir($dir, 0755, true);
+foreach ($storagePaths as $path) {
+    if (!is_dir($path)) {
+        mkdir($path, 0755, true);
     }
 }
 
-// Alihkan cache bootstrap dan Blade views ke /tmp
+// Redirect cache ke /tmp
 putenv('APP_CONFIG_CACHE=/tmp/bootstrap/cache/config.php');
 putenv('APP_EVENTS_CACHE=/tmp/bootstrap/cache/events.php');
 putenv('APP_PACKAGES_CACHE=/tmp/bootstrap/cache/packages.php');
 putenv('APP_ROUTES_CACHE=/tmp/bootstrap/cache/routes.php');
 putenv('APP_SERVICES_CACHE=/tmp/bootstrap/cache/services.php');
 putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+putenv('APP_STORAGE=/tmp/storage');
 
 require __DIR__ . '/../public/index.php';
