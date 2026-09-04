e10797cc79faf8c6d1cfc78c70f6df000398d7fc
Laila1303 <laila.240180177@mhs.unimal.ac.id>
2026-09-05 06:28:41 +0700

    Fix temporary storage path for Vercel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,26 @@
 <?php
+
+// Buat folder storage sementara di /tmp
+$dirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+    '/tmp/bootstrap/cache',
+];
+
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
+// Alihkan cache bootstrap dan Blade views ke /tmp
+putenv('APP_CONFIG_CACHE=/tmp/bootstrap/cache/config.php');
+putenv('APP_EVENTS_CACHE=/tmp/bootstrap/cache/events.php');
+putenv('APP_PACKAGES_CACHE=/tmp/bootstrap/cache/packages.php');
+putenv('APP_ROUTES_CACHE=/tmp/bootstrap/cache/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/bootstrap/cache/services.php');
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+
 require __DIR__ . '/../public/index.php';
