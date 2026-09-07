2bcdfbe3db92df1bf880a88a67c179d8cb1368fd
Laila1303 <laila.240180177@mhs.unimal.ac.id>
2026-09-07 10:43:56 +0700

    Force storage path to /tmp and set log channel to stderr

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-// Siapkan direktori storage writable di /tmp
+// 1. Setup direktori writable di /tmp
 $storage = '/tmp/storage';
 $dirs = [
     $storage . '/framework/views',
@@ -19,9 +19,10 @@ foreach ($dirs as $dir) {
     }
 }
 
-// Set environment variables untuk storage & cache
+// 2. Override Environment Variables
 $_ENV['APP_STORAGE'] = $storage;
 $_ENV['VIEW_COMPILED_PATH'] = $storage . '/framework/views';
+$_ENV['LOG_CHANNEL'] = 'stderr';
 $_ENV['APP_SERVICES_CACHE'] = '/tmp/bootstrap/cache/services.php';
 $_ENV['APP_PACKAGES_CACHE'] = '/tmp/bootstrap/cache/packages.php';
 $_ENV['APP_CONFIG_CACHE'] = '/tmp/bootstrap/cache/config.php';
@@ -30,11 +31,20 @@ $_ENV['APP_EVENTS_CACHE'] = '/tmp/bootstrap/cache/events.php';
 
 putenv("APP_STORAGE={$storage}");
 putenv("VIEW_COMPILED_PATH={$storage}/framework/views");
+putenv("LOG_CHANNEL=stderr");
 putenv("APP_SERVICES_CACHE=/tmp/bootstrap/cache/services.php");
 putenv("APP_PACKAGES_CACHE=/tmp/bootstrap/cache/packages.php");
 putenv("APP_CONFIG_CACHE=/tmp/bootstrap/cache/config.php");
 putenv("APP_ROUTES_CACHE=/tmp/bootstrap/cache/routes.php");
 putenv("APP_EVENTS_CACHE=/tmp/bootstrap/cache/events.php");
 
-// Delegasikan eksekusi penuh ke file bootstrap asli Laravel
-require __DIR__ . '/../public/index.php';
+// 3. Inisialisasi & Paksa Storage Path Laravel
+require __DIR__ . '/../vendor/autoload.php';
+
+/** @var \Illuminate\Foundation\Application $app */
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->useStoragePath($storage);
+
+// 4. Handle Request
+$app->handleRequest(\Illuminate\Http\Request::capture());
